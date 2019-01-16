e9ab33f850016a435f5cf7050f4ee2eed4a0515a
Dobby233Liu <15816141883@163.com>
2019-01-16 16:34:41 +0800

    move

--- Trololo/Form1.Designer.cs
+++ Trololo/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(495, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TROLOLOLOLOLOLOLOLO";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(515, 63);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ROLL THE RICK!!!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Trololo/Form1.cs
+++ Trololo/Form1.cs
@@ -11,9 +11,37 @@ namespace Trololo
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            Rectangle bounds = Screen.GetBounds(Screen.GetBounds(Point.Empty));
+            Random rd = new Random();
+            while (true)
+            {
+                int picXPoint = rd.Next(0, bounds.Right - this.Width);
+                int picYPoint = rd.Next(0, bounds.Height - this.Height);
+                Point ulCorner = new Point(picXPoint, picYPoint);
+                this.Location = ulCorner;
+                Delay(100);
+            }
+        }
+
+        public static void Delay(int mm)
+        {
+            DateTime current = DateTime.Now;
+            while (current.AddMilliseconds(mm) > DateTime.Now)
+            {
+                Application.DoEvents();
+            }
+            return;
         }
     }
 }
